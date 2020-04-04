dd4ae14edc5d555131da46caa1f628a5517b7dc1
takbir miah <62444527+kol9999@users.noreply.github.com>
2020-04-04 13:55:02 +0300

    Update main.cpp

--- Lab4/parallelogram/main.cpp
+++ Lab4/parallelogram/main.cpp
@@ -1,36 +1,11 @@
 #include "interface.h"
 #include "parallelogram.h"
-#include "rectangle.h"
+
 
 int main() {
     // *** Working with rectangles and sets of rectangles ***
 
-    Rectangle rec1, rec2, rec3;
-    std::cout << "=-=- Entering Data for the first rectangle -=-=" << std::endl;
-    rec1.initFromDialog();
-    std::cout << "\n=-=- Entering Data for the second rectangle -=-=" << std::endl;
-    rec2.initFromDialog();
-    std::cout << "\n=-=- Entering Data for the third rectangle -=-=" << std::endl;
-    rec3.initFromDialog();
-
-
-    RectangleSet recSet;
-    recSet.add(&rec1);
-    recSet.add(&rec2);
-    recSet.add(&rec3);
-
-    std::cout << "\n=-=- Printing all details of all rectangles -=-=" << std::endl;
-    recSet.displaySet();
-    std::cout << std::endl;
-    std::cout << "Perimeter Sum: " << recSet.perimeterSum() << std::endl;
-    std::cout << "Area Sum: " << recSet.areaSum() << std::endl;
-    std::cout << "Size in memory sum: " << recSet.sizeSum() << std::endl;
-    recSet.sortByMass();
-
-    std::cout<< std::endl <<"************************************************"<< std::endl;
-
-
-
+ 
 
     //*** Working with segments and sets of parrallelogram ***
     Parallelogram para1, para2, para3;
